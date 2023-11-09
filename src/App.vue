b502d039040401555172e839a5b4e138fc92cc30
alexraduca01 <alex.raduca01@icloud.com>
2023-11-09 16:23:42 +0100

    added MainContent component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <HeaderApp />
+  <MainContent />
 </template>
 
 <script>
+import MainContent from './components/MainContent.vue';
 import HeaderApp from './components/HeaderApp.vue';
   export default {
     components: {
       HeaderApp,
+      MainContent,
     },
     data(){
       return {
---
+++ src/components/MainContent.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="bg-black">
+        <div class="container">
+            <h2 class="p-5 text-white">
+                --&gt; Content goes here &lt;--
+            </h2>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainContent',
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
