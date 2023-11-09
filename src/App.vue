e8f024aa27342b19094dafedb08e0b7ab88e6357
alexraduca01 <alex.raduca01@icloud.com>
2023-11-09 16:16:49 +0100

    added HeaderApp component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <HeaderApp />
 </template>
 
 <script>
+import HeaderApp from './components/HeaderApp.vue';
   export default {
-    
+    components: {
+      HeaderApp,
+    },
+    data(){
+      return {
+
+      }
+    },
   }
 </script>
 
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,91 @@
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center p-3">
+            <div>
+                <img src="img/dc-logo.png" alt="logo">
+            </div>
+            <nav>
+                <ul class="nav">
+                    <li class="nav-item" @click="changeActiveIndex(content.id)" v-for="(content, index) in headerNavbar">
+                        <a class="nav-link text-black text-uppercase" :class="activeElement(content.id)" href="#">{{ content.title }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderApp',
+        data(){
+            return {
+                headerNavbar: [
+                    {
+                        id: 1,
+                        title: 'characters',
+                    },
+                    {
+                        id: 2,
+                        title: 'comics',
+                    },
+                    {
+                        id: 3,
+                        title: 'movies',
+                    },
+                    {
+                        id: 4,
+                        title: 'tv',
+                    },
+                    {
+                        id: 5,
+                        title: 'games',
+                    },
+                    {
+                        id: 6,
+                        title: 'collectibles',
+                    },
+                    {
+                        id: 7,
+                        title: 'videos',
+                    },
+                    {
+                        id: 8,
+                        title: 'fans',
+                    },
+                    {
+                        id: 9,
+                        title: 'news',
+                    },
+                    {
+                        id: 10,
+                        title: 'shop',
+                    },  
+                ],
+                activeIndex: 1,
+            }
+        },
+        methods: {
+            changeActiveIndex(id){
+                for(let i = 0; i < this.headerNavbar.length; i++){
+                    if(this.headerNavbar[i].id === id){
+                        this.activeIndex = i;
+                    };
+                };
+            },
+            activeElement(id){
+                if(this.headerNavbar[this.activeIndex].id === id){
+                    return "is-active";
+                };
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/css/main.scss' as *;
+.is-active {
+    color: $main-color;
+    border-bottom: 5px solid $main-color;
+}
+</style>
