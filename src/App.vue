41e1fde42763e0d23eb02266db53d8e660bf6ebb
alexraduca01 <alex.raduca01@icloud.com>
2023-11-09 18:26:11 +0100

    added SecondFooter component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <MainContent />
   <NavbarComponent />
   <FooterApp />
+  <SecondFooter />
 </template>
 
 <script>
@@ -10,12 +11,14 @@ import HeaderApp from './components/HeaderApp.vue';
 import MainContent from './components/MainContent.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
 import FooterApp from './components/FooterApp.vue';
+import SecondFooter from './components/SecondFooter.vue';
   export default {
     components: {
       HeaderApp,
       MainContent,
       NavbarComponent,
       FooterApp,
+      SecondFooter,
     },
     data(){
       return {
---
+++ src/components/SecondFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="my-container py-5">
+        <div class="container d-flex justify-content-between">
+            <div class="d-flex align-items-center ">
+                <div class="sign-up p-3 fs-5 text-uppercase ">sign up now!</div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center gap-3">
+                <span class="fs-5 text-uppercase ">follow us</span>
+                <img src="img/footer-facebook.png" alt="facebook">
+                <img src="img/footer-twitter.png" alt="twitter">
+                <img src="img/footer-youtube.png" alt="youtube">
+                <img src="img/footer-pinterest.png" alt="pinterest">
+                <img src="img/footer-periscope.png" alt="periscope">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SecondFooter',
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/css/main.scss' as *;
+
+.my-container {
+    background-color: $main-background-color;
+    position: relative;
+    z-index: 1000;
+}
+.sign-up {
+    color: white;
+    border: 2px solid $main-color;
+    cursor: pointer;
+}
+span {
+    color: $main-color;
+    cursor: pointer;
+}
+img {
+    cursor: pointer;
+}
+</style>
